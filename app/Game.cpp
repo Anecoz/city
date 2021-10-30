9f5b669b0238af0605ae4e9515e118ef60dd272b
Anecoz <heilmair5@gmail.com>
2021-10-30 18:07:12 +0200

    Add imgui

--- app/Game.cpp
+++ app/Game.cpp
@@ -3,9 +3,14 @@
 #include "Constants.h"
 #include "FontCache.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
+
 #include "ecs/Engine.h"
 
 #include "systems/InputSystem.h"
+
+#include <iostream>
 
 Game::Game()
   : _window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), GAME_TITLE)
@@ -14,6 +19,8 @@ Game::Game()
 void Game::init()
 {
   fontcache::init();
+  ImGui::SFML::Init(_window);
+  _window.setFramerateLimit(60);
 }
 
 void Game::run()
@@ -22,13 +29,24 @@ void Game::run()
 
   engine.addSystem(std::make_unique<InputSystem>(_window));
 
-  //sf::Clock clock;
+  sf::Clock clock;
   while (_window.isOpen()) {
     _window.clear(sf::Color::Black);
-
-    //sf::Time elapsed = clock.restart();
+    sf::Time elapsed = clock.restart();
     engine.runOnce();
+
+    ImGui::SFML::Update(_window, elapsed);
+
+    ImGui::Begin("Hello, world!");
+    if (ImGui::Button("Look at this pretty button")) {
+      std::cout << "Pressed!" << std::endl;
+    }
+    ImGui::End();
+    
+    ImGui::SFML::Render(_window);
 
     _window.display();
   }
+
+  ImGui::SFML::Shutdown();
 }
--- app/systems/InputSystem.cpp
+++ app/systems/InputSystem.cpp
@@ -1,5 +1,8 @@
 #include "InputSystem.h"
 
+#include "../imgui/imgui.h"
+#include "../imgui/imgui-SFML.h"
+
 #include "../ecs/Engine.h"
 
 #include <iostream>
@@ -12,6 +15,7 @@ void InputSystem::run(ecs::Engine& engine)
 {
   sf::Event event;
   while (_window.pollEvent(event)) {
+    ImGui::SFML::ProcessEvent(event);
     if (event.type == sf::Event::Closed) {
       std::cout << "Closing!" << std::endl;
       _window.close();
